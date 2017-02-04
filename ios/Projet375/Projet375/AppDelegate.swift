67ab5de841a59d13966024578319be6174d6b633
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2017-02-04 13:46:02 -0500

    Login fonctionnel

--- ios/Projet375/Projet375/AppDelegate.swift
+++ ios/Projet375/Projet375/AppDelegate.swift
@@ -27,6 +27,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return SDKApplicationDelegate.shared.application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
