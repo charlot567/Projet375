043c0bd6909160e896a02ec1895eb6d8041e38ab
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2017-02-04 11:22:35 -0500

    Add project

---
+++ ios/Projet375/Projet375/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  Projet375
+//
+//  Created by Charles-Olivier Demers on 17-02-04.
+//  Copyright © 2017 Factory26. All rights reserved.
+//
+
+import UIKit
+
+var kWidth: CGFloat!
+var kHeight: CGFloat!
---
+++ ios/Projet375/Projet375/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Projet375
+//
+//  Created by Charles-Olivier Demers on 17-02-04.
+//  Copyright © 2017 Factory26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        kWidth = self.view.frame.width
+        kHeight = self.view.frame.height
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
