1fb94cf0f962a32b6495288935d2234bf6421278
Alex Morin <alexmorin68@outlook.com>
2017-02-04 12:21:00 -0500

    Commit 1

---
+++ ios/Projet375/Projet375/questionView.swift
@@ -0,0 +1,21 @@
+//
+//  questionView.swift
+//  Projet375
+//
+//  Created by Alex Morin on 17-02-04.
+//  Copyright © 2017 Factory26. All rights reserved.
+//
+
+import UIKit
+
+class questionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
