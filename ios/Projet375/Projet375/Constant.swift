a59601aef61313bee195b202ba3024225f474550
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2017-02-05 07:36:03 -0500

    OK

--- ios/Projet375/Projet375/Constant.swift
+++ ios/Projet375/Projet375/Constant.swift
@@ -19,6 +19,7 @@ let kBlue: UIColor   = UIColor(colorLiteralRed: 85/255, green: 168/255, blue: 21
 let kGreen: UIColor  = UIColor(colorLiteralRed: 124/255, green: 184/255, blue: 69/255, alpha: 1.0)
 let kOrange: UIColor = UIColor(colorLiteralRed: 245/255, green: 144/255, blue: 35/255, alpha: 1.0)
 let kPurple: UIColor = UIColor(colorLiteralRed: 148/255, green: 75/255, blue: 64/255, alpha: 1.0)
+let kBeige: UIColor = UIColor(colorLiteralRed: 224/255, green: 228/255, blue: 204/255, alpha: 1.0)
 
 //Les urls de l'API
 let kBaseUrl:String = "http://79.170.44.147/projet-375.org/API"
